5aac789eb84a0984213504d1adbfeaf36ffd15a1
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2023-02-07 21:06:52 +0100

    imroved the chat ui

---
+++ client/src/components/ActiveUsers.js
@@ -0,0 +1,12 @@
+import "./../styles/ActiveUsers.css";
+
+function ActiveUsers() {
+  return (
+    <div className="active-users-container">
+      <h1 className="side-header-primary">ChatLHR</h1>
+      <h3 className="side-header-secondary">Active users</h3>
+    </div>
+  );
+}
+
+export default ActiveUsers;
---
+++ client/src/components/ChatHeader.js
@@ -0,0 +1,7 @@
+import "./../styles/ChatHeader.css";
+
+function ChatHeader() {
+  return <header className="chat-header">Chat header!</header>;
+}
+
+export default ChatHeader;
